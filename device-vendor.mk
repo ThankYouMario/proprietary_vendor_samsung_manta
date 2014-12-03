58a925f3b7332bb7c3a4d38ef483e7ff33fd89ce
Evan Anderson <evananderson@aospa.co>
2014-12-03 01:26:47 -0500

    Update to include all blobs
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- device-vendor.mk
+++ device-vendor.mk
@@ -8,16 +8,46 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
-implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_STEM := manta/device-partial.mk
+# Samsung blob(s) necessary for manta hardware
+PRODUCT_PACKAGES := \
+    fimc_is_fw \
+    fimc_is_fw2 \
+    mfc_fw \
+    maxtouch \
+    setfile \
+    setfile_4e5 \
+    setfile_6a3 \
+    00060308060501020000000000000000 \
+    020a0000000000000000000000000000 \
+    07060000000000000000000000000000 \
+    ffffffff000000000000000000000005 \
+    libGLES_mali \
+    libmalicore \
+    libRSDriverArm \
+    libbccArm \
+    libstagefright_hdcp
 
-$(call inherit-product-if-exists, vendor/audience/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/samsung/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/widevine/$(LOCAL_STEM))
+# Broadcom blob(s) necessary for Manta hardware
+PRODUCT_PACKAGES := \
+    bcm2079x_firmware \
+    bcm2079x_pre_firmware \
+    bcm43241
+
+# Audience blob(s) necessary for manta hardware
+PRODUCT_PACKAGES := \
+    es305_fw
+
+# Widevine blob(s) necessary for Manta hardware
+PRODUCT_PACKAGES := \
+    libwvdrm_L1 \
+    libWVStreamControlAPI_L1 \
+    libwvm \
+    libdrmwvmplugin \
+    libdrmdecrypt \
+    libwvdrmengine
 
 PRODUCT_RESTRICT_VENDOR_FILES := owner
